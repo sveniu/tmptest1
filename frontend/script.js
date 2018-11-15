5e3a0a7bc89886dac1bb729efe5735d3c5c2ba1a
Sven Ulland <sven.ulland@xeneta.com>
2018-11-15 15:52:43 +0100

    show window.location.href by default

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,13 @@
 const elExecute = document.getElementById("execute")
 const elOutput = document.getElementById("output")
+const elTargetURL = document.getElementById("target_url")
+
+elTargetURL.value = window.location.href + "api/"
 
 elExecute.addEventListener("click", e => {
     e.preventDefault()
 
-    const target_url = document.getElementById("target_url").value
+    const target_url = elTargetURL.value
     fetch(target_url)
         .then(response => response.json())
         .then(json => elOutput.innerText = JSON.stringify(json, null, 4))
