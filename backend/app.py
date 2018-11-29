726c970f8887d37da58803e061c0f6e683ae4722
Sven Ulland <sven.ulland@xeneta.com>
2018-11-29 09:33:08 +0100

    return epoch as seconds, string

--- backend/app.py
+++ backend/app.py
@@ -23,7 +23,7 @@ def application(environ, start_response):
 
     # Populate system environment info.
     data["system"]["time"] = datetime.datetime.utcnow().isoformat()+'Z'
-    data["system"]["time_ms"] = int(time.time() * 1000)
+    data["system"]["time_epoch"] = '%.3f'%time.time()
     data["system"]["cpus"] = []
     try:
         with open("/proc/cpuinfo") as f:
